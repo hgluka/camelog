5d7611c925a5d5f44e37e3c72b2f069d4b77af19
Luka Hadzi-Djokic <luka.hadzi@proton.me>
2025-08-10 16:13:42 +0200

    finish implementing the basic functionality

--- bin/main.ml
+++ bin/main.ml
@@ -1,26 +1,17 @@
 open Core
 
-let transmute (title : string) (filename : string) (template:string) : string =
-  let md_contents = In_channel.with_file filename ~f:(fun ic ->
-    In_channel.input_all ic) in
-  let template_contents = In_channel.with_file template ~f:(fun ic ->
-    In_channel.input_all ic) in
-  List.iter (Util.traverse "test/assets" (fun _ -> false))
-  ~f:(print_endline);
-  Util.md_to_html title md_contents template_contents
-
 let command =
   Command.basic
     ~summary:"A static blog generator written in OCaml"
     ~readme:(fun () -> "More detailed information")
     (let%map_open.Command
-      title =
-        anon ("title" %: string)
-      and filename =
-        anon ("filename" %: Command.Arg_type.create Util.regular_file)
-      and template =
-        anon ("template" %: Command.Arg_type.create Util.regular_file)
+      template =
+        anon ("template" %: string)
+      and input_dir =
+        anon ("input_dir" %: string)
+      and output_dir =
+        anon ("output_dir" %: string)
      in
-     fun () -> print_endline (transmute title filename template))
+     fun () -> Util.transmute template input_dir (fun _s -> false) output_dir)
 
 let () = Command_unix.run ~version:"1.0" command
--- lib/util.ml
+++ lib/util.ml
@@ -6,12 +6,22 @@ type page_type =
   | Other
 
 type page = {
+  page_type: page_type;
   title: string;
-  full_path: string;
-  relative_path: string
+  relative_path: string;
+  input_path: string;
+  output_path: string
 }
 
-let pages (paths : string list) : page list =
+let string_of_ptype pt = match pt with
+  | Markdown -> "Markdown"
+  | Css -> "Css"
+  | Other -> "Other"
+
+let string_of_page p =
+  String.concat ["page: { title = \""; p.title; "\", relative_path = "; p.relative_path; ", type = "; string_of_ptype p.page_type; " }"]
+
+let pages (input_dir: string) (output_dir: string) (paths : string list) : page list =
   List.map paths
     ~f:(fun path ->
         let path_and_ext = Filename.split_extension path in
@@ -23,21 +33,23 @@ let pages (paths : string list) : page list =
           | Some "md" -> "html"
           | Some x -> x
           | None -> "" in
-        let cut_path = Filename.basename (String.chop_prefix_if_exists path ~prefix:input_dir) in
+        let cut_path = String.chop_prefix_if_exists (fst path_and_ext) ~prefix:input_dir in
         {
           page_type = ptype;
-          title = (Filename.basename path);
-          full_path = Caml_unix.realpath path;
-          relative_path = path
+          title = FilePath.basename cut_path;
+          relative_path = FilePath.add_extension cut_path new_ext;
+          input_path = path;
+          output_path = Filename.concat output_dir (FilePath.add_extension cut_path new_ext)
         })
 
-let rec traverse directory exclude =
-  let flat_contents = Sys_unix.ls_dir directory in
+let rec traverse input_dir exclude output_dir =
+  let flat_contents = Sys_unix.ls_dir input_dir in
   let contents = List.map (List.filter flat_contents ~f:(fun p -> not (exclude p)))
       ~f:(fun p ->
-        let full_path = Filename.concat directory p in
+        let full_path = Filename.concat input_dir p in
         match Sys_unix.is_directory full_path with
-        | `Yes -> traverse full_path exclude
+        | `Yes -> (if not (String.is_empty output_dir)
+                   then (FileUtil.mkdir ~parent:true ~mode:(`Octal 0o755) (Filename.concat output_dir p) )); traverse full_path exclude output_dir
         | _ -> [full_path])
       in
   List.concat contents
@@ -56,3 +68,17 @@ let md_to_html (title:string) (md:string) (template:string) : string =
   let buffer_add_title = Cmarkit_html.buffer_add_html_escaped_string in
   Printf.kbprintf Buffer.contents (Buffer.create 1024)
     html_template buffer_add_title title buffer_add_doc doc
+
+let process (pages: page list) (template_path: string) =
+  let template = In_channel.with_file template_path ~f:(fun ic ->
+            In_channel.input_all ic) in
+  List.map pages
+    ~f:(fun page ->
+        let page_contents = In_channel.with_file page.input_path ~f:(fun ic ->
+            In_channel.input_all ic) in
+        match page.page_type with
+        | Markdown -> Out_channel.write_all page.output_path ~data:(md_to_html page.title page_contents template)
+        | _ -> Out_channel.write_all page.output_path ~data:page_contents)
+
+let transmute template input_dir exclude output_dir =
+  ignore (process (pages input_dir output_dir (traverse input_dir exclude output_dir)) template)
